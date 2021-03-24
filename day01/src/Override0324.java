b4ed39b25f00950007cae1a26692ee3d3927c7ca
Dejago <80389771+Dejago@users.noreply.github.com>
2021-03-24 17:52:48 +0800

    0324 UpdateB

--- day01/src/Override0324.java
+++ day01/src/Override0324.java
@@ -1,21 +1,31 @@
-public class Override0324 extends Overload0324{
+public class Override0324 {
     public static void main(String[] args) {
 
-        System.out.println(getValue("KFC"));
-        System.out.println(new Override0324().getValue(new Integer(25)));
+        Father father = new Father();
+        System.out.println(father.printValue(20));
+        Child child = new Child();
+        System.out.println(child.printValue(45));
+
     }
 
+}
 
+class Father {
 
-    //静态方法无法重写，添加@Override注解会报错
-    //而且由于静态方法是通过类名调用的，会根据调用的类来区分方法，即使重写也达不到多态的目的
-    static String getValue(String s) {
-        return new StringBuilder().append("--").append(s).toString();
+    Object printValue(int i) {
+        System.out.println("向父类传入了参数" + i +"，并将其乘以2");
+        i *= 2;
+        return i;
     }
+}
 
+class Child extends Father {
+
+    //重写要求方法名和传入参数完全一样，返回类型可以不同但必须是被重写方法返回类型的子类
     @Override
-    String getValue(Integer value) {
-        value *= 10;
-        return value.toString();
+    String printValue(int i) {
+        System.out.println("向子类传入了参数" + i +"，再加15");
+        i += 15;
+        return String.valueOf(i);
     }
 }
